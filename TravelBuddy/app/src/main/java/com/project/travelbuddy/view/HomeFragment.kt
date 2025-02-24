1f2cba6d979a20b7763f3235e1425cb7cd6f1fd1
Hadar Almagor <104783454+HadarAlmagor@users.noreply.github.com>
2025-02-24 18:52:19 +0200

    post creation & images saved in firebase storage

--- TravelBuddy/app/src/main/java/com/project/travelbuddy/view/HomeFragment.kt
+++ TravelBuddy/app/src/main/java/com/project/travelbuddy/view/HomeFragment.kt
@@ -3,25 +3,33 @@ package com.project.travelbuddy.view
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
-import com.google.firebase.auth.FirebaseAuth
 import com.project.travelbuddy.R
+import com.project.travelbuddy.adapter.TravelPostAdapter
 import com.project.travelbuddy.databinding.FragmentHomeBinding
+import com.project.travelbuddy.viewmodel.TravelViewModel
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
     private lateinit var binding: FragmentHomeBinding
-    private lateinit var auth: FirebaseAuth
+    private lateinit var viewModel: TravelViewModel
+    private lateinit var adapter: TravelPostAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding = FragmentHomeBinding.bind(view)
-        auth = FirebaseAuth.getInstance()
+        viewModel = ViewModelProvider(this).get(TravelViewModel::class.java)
 
-        binding.logoutBtn.setOnClickListener {
-            auth.signOut()
-            findNavController().navigate(R.id.action_homeFragment_to_signInFragment)
+        viewModel.travelPosts.observe(viewLifecycleOwner) { posts ->
+            adapter = TravelPostAdapter(posts)
+            binding.recyclerView.adapter = adapter
+        }
+
+        binding.fabAddPost.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_addPostFragment)
         }
     }
 }
+
